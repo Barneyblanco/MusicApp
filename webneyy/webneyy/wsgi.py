a76a5fcccc026a5b45972baeca07fce2325692b6
Barneyblanco <41872411+Barneyblanco@users.noreply.github.com>
2019-03-01 14:33:51 -0600

    Add files via upload

---
+++ webneyy/webneyy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webneyy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webneyy.settings')
+
+application = get_wsgi_application()
